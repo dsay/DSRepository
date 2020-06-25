262108a6045d963cafb1e36f28c94022aeebd248
Dmitriy Sai <dmitriy.sai2013@gmail.com>
2020-06-25 16:03:48 +0300

    add exclude

--- Package.swift
+++ Package.swift
@@ -7,9 +7,7 @@ let package = Package(
     name: "Repository",
     platforms: [.iOS(.v11)],
     products: [
-        .library(
-            name: "SwiftRepository",
-            targets: ["Repository"])
+        .library(name: "SwiftRepository", targets: ["Repository"])
     ],
     dependencies: [
         .package(url: "https://github.com/realm/realm-cocoa.git", from: "5.1.0"),
@@ -18,18 +16,8 @@ let package = Package(
         .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", from: "4.2.0")
     ],
     targets: [
-        .target(
-            name: "Repository",
-            dependencies: [
-                .package(url: "https://github.com/realm/realm-cocoa.git", from: "5.1.0"),
-                .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.13.2"),
-                .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.2.1"),
-                .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", from: "4.2.0")
-            ],
-            path: "Sources"),
-        .testTarget(
-            name: "RepositoryTests",
-            dependencies: ["Repository"]),
+        .target(name: "Repository", dependencies: [], path: "Sources", exclude: ["Example", "Tests", "Package.relolved", "SwiftRepository.podspec"]),
+        .testTarget(name: "RepositoryTests", dependencies: ["Repository"]),
     ],
     swiftLanguageVersions: [.v5]
 )
