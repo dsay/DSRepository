3b3f9f827a03593e19126793a96fa4d9457e95c8
Dmitriy Sai <dmitriy.sai2013@gmail.com>
2020-06-25 16:19:09 +0300

    resolve

--- Package.swift
+++ Package.swift
@@ -5,18 +5,24 @@ import PackageDescription
 
 let package = Package(
     name: "Repository",
-    platforms: [.iOS(.v11)],
+    platforms: [.macOS(.v10_12),
+                .iOS(.v11),
+                .tvOS(.v10),
+                .watchOS(.v3)],
     products: [
         .library(name: "SwiftRepository", targets: ["Repository"])
     ],
     dependencies: [
-        .package(url: "https://github.com/realm/realm-cocoa.git", from: "5.1.0"),
-        .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.13.2"),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.2.1"),
-        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", from: "4.2.0")
+        .package(name: "Realm", url: "https://github.com/realm/realm-cocoa.git", from: "5.1.0"),
+        .package(name: "PromiseKit", url: "https://github.com/mxcl/PromiseKit.git", from: "6.13.2"),
+        .package(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire.git", from: "5.2.1"),
+        .package(name: "ObjectMapper", url: "https://github.com/tristanhimmelman/ObjectMapper.git", from: "4.2.0")
     ],
     targets: [
-        .target(name: "Repository", dependencies: [], path: "Sources", exclude: ["Example", "Tests", "Package.relolved", "SwiftRepository.podspec"]),
+        .target(name: "Repository",
+                dependencies: ["Alamofire", "Realm", "ObjectMapper", "PromiseKit"],
+                path: "Sources",
+                exclude: ["Example", "Tests", "Package.relolved", "SwiftRepository.podspec"]),
         .testTarget(name: "RepositoryTests", dependencies: ["Repository"]),
     ],
     swiftLanguageVersions: [.v5]
